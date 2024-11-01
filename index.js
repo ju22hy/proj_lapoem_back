c9403b6caab1ca1fcd41e423bb485a218b9594fe
bonse koo <bonse88@hs.ac.kr>
2024-11-01 17:17:20 +0900

    회원가입, 로그인 구현

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = '8080';
+const cookieParser = require('cookie-parser');
+const PORT = '8002';
 
 const app = express(); //express 모듈을 사용하기 위해 app 변수에 할당
 
 app.use(express.json()); //express 모듈의 json()메소드를 사용한다.
+app.use(cookieParser()); // 쿠키 파서 추가
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3002',
     credentials: true,
   })
 );
@@ -19,6 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(require('./routes/getRoutes'));
+app.use(require('./routes/postRoutes')); // postRoutes 경로 설정
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
