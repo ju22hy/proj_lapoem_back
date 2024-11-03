631b7721353739b31d8f5e9b1e0a3d551cda2a1e
lollipopwyn <rwewine@gmail.com>
2024-11-03 17:56:10 +0900

    241103_feat_common_검색 기능 api 생성 _나라

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const { getBookList } = require('../controllers/getBookList');
+const { getSearchBooks } = require('../controllers/getSearchBooks');
 const { verifyToken } = require('../controllers/authController');
 
 router.get('/book-list', getBookList);
+router.get('/search-books', getSearchBooks);
 
 // 토큰 검증 라우트
 router.get('/verify', verifyToken);
