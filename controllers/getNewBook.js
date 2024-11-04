da27405e93b6199b735eb34716ef32b546aba039
sora <ju22hy@gmail.com>
2024-11-04 18:06:57 +0900

    241104_feat_main_신간도서 불러오기 수정_유소라

--- controllers/getNewBook.js
+++ controllers/getNewBook.js
@@ -16,12 +16,12 @@ exports.getNewBook = async (req, res) => {
       LEFT JOIN 
         book_review ON book.book_id = book_review.book_id      -- LEFT JOIN을 사용해 리뷰가 없는 책도 포함
       WHERE 
-        AND book.publish_date IS NOT NULL
+        book.publish_date IS NOT NULL                          -- 출판일이 NULL이 아닌 책만 포함
       GROUP BY 
         book.book_id, book.book_cover, book.book_title, book.book_author, book.book_publisher
       ORDER BY 
         book.publish_date DESC
-      LIMIT 5
+      LIMIT 5                                                -- 최근 5권만 가져오기
     `;
 
     const { rows: latestBooks } = await database.query(query);
@@ -35,7 +35,7 @@ exports.getNewBook = async (req, res) => {
     return res.status(200).json(latestBooks);
   } catch (error) {
     console.error("Error fetching latest books:", error);
-    res
+    return res
       .status(500)
       .json({ message: "Failed to fetch latest books", error: error.message });
   }
