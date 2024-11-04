bfcfd381c285d08b029ca577e81be4abf93a7b8b
lollipopwyn <rwewine@gmail.com>
2024-11-04 21:56:48 +0900

    241104_feat_booklist_책 리뷰 get 백 구현_나라

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,29 +1,32 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   getBookList,
   getBookByCategory,
   getAllCategories,
-} = require("../controllers/getBookList");
-const { getSearchBooks } = require("../controllers/getSearchBooks");
-const { verifyToken } = require("../controllers/authController");
-const { getNewBook } = require("../controllers/getNewBook");
-const { getBestBook } = require("../controllers/getBestBook");
+} = require('../controllers/getBookList');
+const { getBookReviews } = require('../controllers/getBookReviews');
+const { getSearchBooks } = require('../controllers/getSearchBooks');
+const { verifyToken } = require('../controllers/authController');
+const { getNewBook } = require('../controllers/getNewBook');
+const { getBestBook } = require('../controllers/getBestBook');
 const {
   getCommunityPosts,
   getCommunityPostById,
-} = require("../controllers/communityController");
+} = require('../controllers/communityController');
 
-router.get("/book-list", getBookList);
-router.get("/community", getCommunityPosts);
-router.get("/community/:postId", getCommunityPostById);
-router.get("/search-books", getSearchBooks);
-router.get("/search-category", getBookByCategory);
-router.get("/all-categories", getAllCategories);
-router.get("/new-book", getNewBook);
-router.get("/best-book", getBestBook);
+// get Url
+router.get('/book-list', getBookList);
+router.get('/book-list/:bookId/reviews', getBookReviews);
+router.get('/community', getCommunityPosts);
+router.get('/community/:postId', getCommunityPostById);
+router.get('/search-books', getSearchBooks);
+router.get('/search-category', getBookByCategory);
+router.get('/all-categories', getAllCategories);
+router.get('/new-book', getNewBook);
+router.get('/best-book', getBestBook);
 
 // 토큰 검증 라우트
-router.get("/verify", verifyToken);
+router.get('/verify', verifyToken);
 
 module.exports = router;
