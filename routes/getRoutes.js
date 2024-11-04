eae6ba61d3579d24cb10209364fb61dc3c4e97fd
sora <ju22hy@gmail.com>
2024-11-04 19:45:12 +0900

    241104_feat_msin_book 테이블 트리거 적용 및 베스트셀러 불러오기_유소라

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   getBookList,
   getBookByCategory,
   getAllCategories,
-} = require('../controllers/getBookList');
-const { getSearchBooks } = require('../controllers/getSearchBooks');
-const { verifyToken } = require('../controllers/authController');
-const { getNewBook } = require('../controllers/getNewBook');
+} = require("../controllers/getBookList");
+const { getSearchBooks } = require("../controllers/getSearchBooks");
+const { verifyToken } = require("../controllers/authController");
+const { getNewBook } = require("../controllers/getNewBook");
+const { getBestBook } = require("../controllers/getBestBook");
 const {
   getCommunityPosts,
   getCommunityPostById,
-} = require('../controllers/communityController');
+} = require("../controllers/communityController");
 
-router.get('/book-list', getBookList);
-router.get('/community', getCommunityPosts);
-router.get('/community/:postId', getCommunityPostById);
-router.get('/search-books', getSearchBooks);
-router.get('/search-category', getBookByCategory);
-router.get('/all-categories', getAllCategories);
-router.get('/new-book', getNewBook);
+router.get("/book-list", getBookList);
+router.get("/community", getCommunityPosts);
+router.get("/community/:postId", getCommunityPostById);
+router.get("/search-books", getSearchBooks);
+router.get("/search-category", getBookByCategory);
+router.get("/all-categories", getAllCategories);
+router.get("/new-book", getNewBook);
+router.get("/best-book", getBestBook);
 
 // 토큰 검증 라우트
-router.get('/verify', verifyToken);
+router.get("/verify", verifyToken);
 
 module.exports = router;
