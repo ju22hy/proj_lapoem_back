a6297e29c05516dc2027bd42de9d2f488c4a8350
lollipopwyn <rwewine@gmail.com>
2024-11-04 21:09:54 +0900

    241104_feat_booklist_책에 대한 리뷰 post 백 구현_나라

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,22 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   joinUser,
   loginUser,
   logoutUser,
-} = require("../controllers/authController"); // authController를 사용
-const communityController = require("../controllers/communityController");
-const { registerBestSeller } = require("../controllers/postBestBook");
+} = require('../controllers/authController'); // authController를 사용
+const communityController = require('../controllers/communityController');
+const { registerBestSeller } = require('../controllers/postBestBook');
+const {
+  postBookReview,
+  verifyToken,
+} = require('../controllers/postBookReview');
 
 // 회원가입 라우트 설정
-router.post("/join", joinUser);
+router.post('/join', joinUser);
 // 로그인 라우트 설정
-router.post("/login", loginUser);
+router.post('/login', loginUser);
 // 로그아웃 라우트 설정
-router.post("/logout", logoutUser); // 추가된 부분
+router.post('/logout', logoutUser); // 추가된 부분
 // 커뮤니티 생성 라우트 설정
-router.post("/community", communityController.createCommunityPost);
+router.post('/community', communityController.createCommunityPost);
 // 베스트셀러 등록
-router.post("/register-best", registerBestSeller);
+router.post('/register-best', registerBestSeller);
+// 책 리뷰 작성
+router.post('/book-list/:bookId/reviews', verifyToken, postBookReview);
 
 module.exports = router;
