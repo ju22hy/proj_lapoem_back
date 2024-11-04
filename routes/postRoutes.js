74a02e08d4e4d322a1dda9307a719ffba61bfdaf
bonse koo <bonse88@hs.ac.kr>
2024-11-04 16:16:27 +0900

    241104_feat_authController_로그아웃_추가_구본세

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { joinUser, loginUser } = require('../controllers/authController'); // authController를 사용
+const { joinUser, loginUser, logoutUser } = require('../controllers/authController'); // authController를 사용
 const communityController = require('../controllers/communityController');
 
 // 회원가입 라우트 설정
 router.post('/join', joinUser);
 // 로그인 라우트 설정
 router.post('/login', loginUser);
-
+// 로그아웃 라우트 설정
+router.post('/logout', logoutUser); // 추가된 부분
+// 커뮤니티 생성 라우트 설정
 router.post('/community', communityController.createCommunityPost);
 
 module.exports = router;
