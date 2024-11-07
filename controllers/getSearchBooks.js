4f8b255a3b901c01a83c9891cf8dd7c3ef2b0e80
lollipopwyn <rwewine@gmail.com>
2024-11-07 15:46:55 +0900

    241107_chore_data_북리스트, 키워드 검색, 필터 검색 데이터 보완_나라

--- controllers/getSearchBooks.js
+++ controllers/getSearchBooks.js
@@ -9,14 +9,40 @@ exports.getSearchBooks = async (req, res) => {
   try {
     // 1. 제목과 저자에서 검색어와 일치하는 도서 찾기
     let query = `
-      SELECT * FROM book    
-      WHERE book_title ILIKE $1 OR book_author ILIKE $1
-      ORDER BY 
-      CASE WHEN is_book_best = true THEN 1 ELSE 0 END DESC,  -- 베스트셀러 우선 출력
-      CASE WHEN book_author = '한강' THEN 1 ELSE 0 END DESC,  -- '한강' 작가의 책 우선 출력
-      (LENGTH(book_title) - LENGTH(REPLACE(book_title, $1, ''))) +
-      (LENGTH(book_author) - LENGTH(REPLACE(book_author, $1, ''))) DESC
-      LIMIT $2 OFFSET $3
+      SELECT 
+        b.book_id,
+        b.book_title,
+        b.book_cover,
+        b.book_author,
+        b.book_publisher,
+        b.genre_tag_name,
+        b.isbn,
+        b.book_description,
+        b.book_price,
+        b.publish_date,
+        b.genre_tag_id,
+        b.is_book_best,
+        b.book_status,
+        CASE 
+            WHEN AVG(CASE WHEN br.review_status = 'active' THEN br.rating END) IS NULL THEN 0 
+            ELSE ROUND(AVG(CASE WHEN br.review_status = 'active' THEN br.rating END), 1) 
+        END AS average_rating, -- 'active' 상태인 리뷰만 평점 계산
+        COUNT(CASE WHEN br.review_status = 'active' THEN br.rating END) AS review_count, -- 'active' 상태인 리뷰만 개수 계산
+        CASE WHEN b.is_book_best = true THEN 1 ELSE 0 END DESC,  -- 베스트셀러 우선 출력
+        CASE WHEN b.book_author = '한강' THEN 1 ELSE 0 END DESC,  -- '한강' 작가의 책 우선 출력
+        (LENGTH(b.book_title) - LENGTH(REPLACE(b.book_title, $1, ''))) +
+        (LENGTH(b.book_author) - LENGTH(REPLACE(b.book_author, $1, ''))) DESC
+    FROM book AS b
+    LEFT JOIN book_review AS br ON b.book_id = br.book_id -- book_review 테이블과 조인
+    WHERE (b.book_title ILIKE $1 OR b.book_author ILIKE $1) 
+    AND b.book_status IS NOT false  -- book_status가 false인 책을 제외
+    GROUP BY b.book_id
+    ORDER BY 
+        CASE WHEN b.is_book_best = true THEN 1 ELSE 0 END DESC,  -- 베스트셀러 우선 출력
+        CASE WHEN b.book_author = '한강' THEN 1 ELSE 0 END DESC,  -- '한강' 작가의 책 우선 출력
+        (LENGTH(b.book_title) - LENGTH(REPLACE(b.book_title, $1, ''))) +
+        (LENGTH(b.book_author) - LENGTH(REPLACE(b.book_author, $1, ''))) DESC
+    LIMIT $2 OFFSET $3
     `;
 
     // 쿼리 실행
